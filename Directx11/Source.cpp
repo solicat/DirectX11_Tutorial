246a6d4ac6cc55fa9d13e409893838a2cb07f05f
solicat <giant3380@gmail.com>
2021-04-09 17:13:49 +0900

    Initial commit

---
+++ Directx11/Source.cpp
@@ -0,0 +1,13 @@
+#include "ErrorLogger.h"
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "DirectXTK.lib")
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
+{
+	ErrorLogger::Log(E_INVALIDARG, "Test Message");
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# C++ DirectX 11 Engine Tutorials
+
+https://www.youtube.com/playlist?list=PLcacUGyBsOIBlGyQQWzp6D1Xn6ZENx9Y2
